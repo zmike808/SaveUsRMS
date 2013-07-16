e335813b42b03a1cce6240c9cd7163276abe3a9c
sarbos <ssarbora@gmail.com>
2013-07-16 12:36:19 -0400

    -record additional functionality

---
+++ RSRD/RecordTextBox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RSRD
+{
+    public class RecordTextBox : System.Windows.Forms.TextBox
+    {
+    
+        //the record that it is attached to, and the index it is placed in the records values list
+        public RecordTextBox(Record r, int index) { }
+
+        public RecordTextBox() { }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            this.ResumeLayout(false);
+        }
+
+    }
+}
---
+++ RSRD/UserControl1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RSRD
+{
+    public partial class UserControl1 : UserControl
+    {
+
+
+        Record parentRecord;
+        int recordIndex;
+        FieldBox f;
+
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        public UserControl1(Record r, int index, FieldBox f) 
+        {
+            parentRecord = r;
+            recordIndex = index;
+            InitializeComponent();
+            label1.Text = f.label;
+        }
+
+    }
+}

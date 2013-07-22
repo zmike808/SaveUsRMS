eed1186ec35bd5ce7c44915fd406babfc64ffe81
Michael Zemsky <zmike808@gmail.com>
2013-07-21 22:07:18 -0400

    - Fixed minor typos/errors in MySQL.cs createTable() - Fixed up some minor issues in the hardcoded testing
    Signed-off-by: Michael Zemsky <zmike808@gmail.com>

--- RSRD/Program.cs
+++ RSRD/Program.cs
@@ -24,12 +24,17 @@ namespace RSRD
         {
             //connect
             //ROOT, ROOT IS DEFAULT USERNAME AND PASSWORD, CHANGE TO YOUR OWN SETTINGS IF NEEDED.
-            MySQLHandler db = new MySQLHandler("localhost", "test", "root", "root");
+            MySQLHandler db = new MySQLHandler("localhost", "root", "root");
 
             #region database setup (DO NOT EDIT)
             if (db.sendCommand("CREATE DATABASE IF NOT EXISTS test"))
             {
-                db.sendCommand("CREATE  TABLE IF NOT EXISTS `Animal` (`ID` INT NOT NULL AUTO_INCREMENT ,`name` TEXT NULL DEFAULT NULL ,  `DateofBirth` DATETIME NULL DEFAULT NULL ,  `image` LONGBLOB NULL DEFAULT NULL ,  `estimate` TINYINT(1) NULL DEFAULT NULL ,  `vaccination` DATETIME NULL DEFAULT NULL ,  `sterilized` TINYINT(1) NULL DEFAULT NULL ,  `female` TINYINT(1) NULL DEFAULT NULL ,  `color` TEXT NULL DEFAULT NULL ,  `size` TEXT NULL DEFAULT NULL ,  `breed` TEXT NULL DEFAULT NULL ,  `crossbreed` TEXT NULL DEFAULT NULL ,  `location` TEXT NULL DEFAULT NULL ,  `owner` TEXT NULL DEFAULT NULL ,  `notes` TEXT NULL DEFAULT NULL ,  PRIMARY KEY (`ID`) )");
+                db.sendCommand("USE test");
+                db.sendCommand("CREATE TABLE IF NOT EXISTS `Animal` (`ID` INT NOT NULL AUTO_INCREMENT ,`name` TEXT NULL DEFAULT NULL ,  `DateofBirth` DATETIME NULL DEFAULT NULL ,  `image` LONGBLOB NULL DEFAULT NULL ,  `estimate` TINYINT(1) NULL DEFAULT NULL ,  `vaccination` DATETIME NULL DEFAULT NULL ,  `sterilized` TINYINT(1) NULL DEFAULT NULL ,  `female` TINYINT(1) NULL DEFAULT NULL ,  `color` TEXT NULL DEFAULT NULL ,  `size` TEXT NULL DEFAULT NULL ,  `breed` TEXT NULL DEFAULT NULL ,  `crossbreed` TEXT NULL DEFAULT NULL ,  `location` TEXT NULL DEFAULT NULL ,  `owner` TEXT NULL DEFAULT NULL ,  `notes` TEXT NULL DEFAULT NULL ,  PRIMARY KEY (`ID`) )");
+            }
+            else 
+            { 
+                db.sendCommand("USE test"); 
             }
             #endregion
 
@@ -40,11 +45,7 @@ namespace RSRD
                  list1.Add("string");
             }
 
-            if (!db.createTable("test1", list1))
-            {
-                Debug.WriteLine("TABLE CREATION FAILED, PROBABLY ALREADY EXISTS. NO WORRIES THO, JUST SAYIN");
-            }
-
+            db.createTable("test1", list1);
 
             List<string> list2 = new List<string>();
             string[] q = {"aaa","bbb","ccc"};
